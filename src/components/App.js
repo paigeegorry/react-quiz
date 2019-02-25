bfbd8f3390db58ca0613cf8b2f0c5415ff8cfdde
Paige Gorry <paigeegorry@gmail.com>
2019-02-25 13:15:14 -0800

    text field written and updating state

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    text: '',
+    fontColor: '',
+    backgroundColor: ''
+  }
+  
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
     return (
-      <h1>Hello!</h1>
+      <>
+        <h1>Hello!</h1>
+        <label>
+          Text: 
+          <input type="text" value={this.state.text} onChange={this.handleChange}></input>
+        </label>
+      </>
     )
   }
 }
