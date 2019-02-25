841e7233c9e2a5d29cd54bae0af887e2ddd068ca
Paige Gorry <paigeegorry@gmail.com>
2019-02-25 13:47:32 -0800

    eslint forgot a semi colon

--- src/components/App.js
+++ src/components/App.js
@@ -18,12 +18,12 @@ class App extends Component {
     const { text, fontColor, backgroundColor } = this.state;
     return (
       <>
-        <h1>Hello!</h1>
+        <h1>Font Customizer</h1>
         <TextSelector text={text} handleChange={this.handleChange} />
         <ColorSelector fontColor={fontColor} backgroundColor={backgroundColor} handleChange={this.handleChange} />
         <Shape text={text} fontColor={fontColor} backgroundColor={backgroundColor} />
       </>
-    )
+    );
   }
 }
 
